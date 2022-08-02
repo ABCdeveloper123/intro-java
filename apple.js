f0dc05ff3727a426ab7e8ef1d3cf89a2f65c9493
ABCdeveloper123 <sohaghasun4@gmail>
2022-08-03 02:31:18 +0600

    end

---
+++ apple.js
@@ -0,0 +1,70 @@
+// var give = 1000;
+// var apple = 400;
+// var orang = 300;
+// var money = apple + orang;
+// console.log(money);
+// var getBack = give - money;
+// console.log(getBack);
+
+// var math = 90;
+// var biology = 25;
+// var chemistry = 65;
+// var physics = 35;
+// var bangla = 99.50;
+
+// var total = math + biology + chemistry + physics + bangla;
+// console.log(total);
+// var avaraj = total / 5;
+// var avaraj2 = avaraj.toFixed(2);
+// console.log(avaraj2);
+
+
+// var num = 119;
+// var reminder = num % 5;
+// console.log(reminder);
+
+
+// var fruits = ['apple', 'banna', 'orang'];
+// var bannas = fruits.indexOf('banna');
+// fruits[bannas] = 'mango';
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.push('water')
+// console.log(fruits);
+
+
+// var num1 = 13;
+// var num2 = 8;
+// var num3 = 1;
+
+// if (num1 > num2) {
+//     if (num1 > num3) {
+//         console.log(num1);
+//     }
+//     else {
+//         console.log(num3);
+//     }
+// }
+// else {
+//     if (num2 > num3) {
+//         console.log(num2);
+//     }
+//     else {
+//         console.log(num3);
+//     }
+
+
+// }
+
+
+
+var slid1 = 9;
+var slid2 = 12;
+var slid3 = 9;
+
+if (
+    slid1 == slid2 || slid2 == slid3 || slid1 == slid3
+) {
+    console.log('angle');
+}
